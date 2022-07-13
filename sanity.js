7a2772e9b0eefa30082bdaf9f93969e489de9b4a
Brandon Cieplinski <71282502+b-cieplinski@users.noreply.github.com>
2022-07-13 10:27:48 +0200

    Sanity update

--- sanity.js
+++ sanity.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client"
 import imageUrlBuilder from "@sanity/image-url"
 
 const client = sanityClient ({
-    projectId: "tvls27mo",
+    projectId: "",
     dataset: "production",
     useCdn: true,
     apiVersion: "2021-10-21"
